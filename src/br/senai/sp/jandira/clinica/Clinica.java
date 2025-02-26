2bea991aecac1003b29c8a865d0d4d6fade908fa
ANDRÉ ROBERTO TAVARES <25132696@CFP127ALUNO.LOCAL>
2025-02-26 11:24:47 -0300

    Métodos get e set iniciados com cálculo do IMC

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -8,19 +8,25 @@ public class Clinica {
 
 	public static void main(String[] args) {
 		
+		int a = 8;
+		int b = 8;
+		boolean teste = a == b;
+		int x = a - b;
+		System.out.println(teste);
+		
 		System.out.println("Hello World");
 		
 		// Criando um objeto paciente
 		Paciente p1 = new Paciente();
-		p1.nome = "Ana Maria";
-		p1.altura = 1.65;
+		p1.setNome("Ana Maria");
+		p1.setAltura(1.65);
 		p1.peso = 51;
 		p1.telefone = "(11)99999-8888";
 		p1.dataNascimento = LocalDate.of(2003, 7, 15);
 		
 		Paciente p2 = new Paciente();
-		p2.nome = "André Roberto Tavares";
-		p2.altura = 1.78;
+		p2.setNome("André Roberto Tavares");
+		p2.setAltura(1.78);
 		p2.peso = 68;
 		p2.telefone = "(11) 99989-4817";
 		p2.dataNascimento = LocalDate.of(2007, 9, 29);
